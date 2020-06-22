667dccacb42c67a3303fb21d362a52018460a161
wllysx <wllysx@163.com>
2020-06-22 16:06:10 +0800

    2020年6月22日16:06:04

---
+++ app/src/main/java/com/wll/myproject/gitframework/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wll.myproject.gitframework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        Log.i( "wll11", "1" );
+    }
+}
