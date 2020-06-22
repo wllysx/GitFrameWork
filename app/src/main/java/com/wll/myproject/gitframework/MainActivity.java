7bff913028e266155f511951e9abe329f2b74f23
wllysx <wllysx@163.com>
2020-06-22 16:30:26 +0800

    2020年6月22日16:30:20

--- app/src/main/java/com/wll/myproject/gitframework/MainActivity.java
+++ app/src/main/java/com/wll/myproject/gitframework/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
         Log.i( "wll11", "1" );
+        Log.i( "wll11", "1" );
     }
 }
